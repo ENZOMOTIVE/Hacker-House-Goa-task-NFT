5586a63839f231c3d1270afeb23e19083d158c47
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-07-14 09:24:38 +0530

    initial-commit

--- Junkyard/src/components/Navbar.jsx
+++ Junkyard/src/components/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+    <nav className="w-full flex md:justify-center justify-between items-center p-4 bg-gradient-to-r from-violet-700 to-pink-500">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
         <img src={logo} alt="logo" className="sm:w-10 lg:w-24 cursor-pointer" />
       </div>
@@ -88,7 +88,7 @@ const Navbar = () => {
         {toggleMenu && (
           <ul
             className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-            flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
+            flex flex-col justify-start items-end rounded-md bg-gradient-to-r from-violet-700 to-pink-500 text-white animate-slide-in"
           >
             <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
             {["Home", "Create Waste", "Explore", "About"].map(
