e118f113a4c2a4fdca84c733f5583e5f6adeaaf6
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-07-14 09:50:15 +0530

    style-commit

--- Junkyard/src/components/Welcome.jsx
+++ Junkyard/src/components/Welcome.jsx
@@ -3,26 +3,26 @@ import React from "react";
 const Welcome = () => {
   return (
     <div className="flex w-full flex-row flex-col justify-center items-center bg-gradient-to-r from-violet-700 to-pink-500 p-10">
-      <div className="flex flex-row flex-col items-start justify-between md:p-20 px-4">
-        <div className="flex flex-1 justify-start items-start flex-col mr-10">
-          <h1 className="text-5xl sm:text-7xl text-white py-1 font-semibold">
+      <div className="flex flex-row flex-col items-center justify-between md:p-20 px-4 text-center">
+        <div className="flex flex-1 justify-center items-center flex-col mb-10">
+          <h1 className="text-6xl sm:text-8xl text-white py-1 font-semibold">
             <span className="text-pink-200">Junkyard </span>
             <span className="text-pink-300">3.O</span>
           </h1>
-          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-4xl">
-            Waste Recycling & Sustainability
-            <br />
-            Help our Environment
-            <br />
-            and get rewarded
-          </p>
-          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-2xl">
-            Connect your wallet, submit details of your
-            <br />
-            environmental waste, recyclers go to the
-            <br />
-            marketplace and recycle waste close to their location...
-          </p>
+          <div className="text-center mt-5 text-white font-light max-w-4xl">
+            <p className="text-4xl leading-tight">
+              Waste Recycling & Sustainability,
+              <br />
+              Help our Environment,
+            </p>
+            <p className="text-4xl leading-tight">
+              and get rewarded
+            </p>
+            <p className="text-2xl mt-5 leading-tight">
+              Connect your wallet, submit details of your environmental waste,
+              recyclers go to the marketplace and recycle waste close to their location...
+            </p>
+          </div>
         </div>
       </div>
     </div>
