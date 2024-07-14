95a2f47beb252aebef7b392d2045801738ef01d7
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-07-14 09:40:23 +0530

    some styling

--- Junkyard/src/App.jsx
+++ Junkyard/src/App.jsx
@@ -3,18 +3,9 @@ import { Navbar, Welcome } from "./components";
 
 const App = () => (
   <div className="min-h-screen">
-    <div className="gradient-bg-welcome">
+    <div className="bg-navbar">
       <Navbar />
       <Welcome />
-    </div>
-    <div className="gradient-bg-services">
-      {/* Your services section */}
-    </div>
-    <div className="gradient-bg-transactions">
-      {/* Your transactions section */}
-    </div>
-    <div className="gradient-bg-footer">
-      {/* Your footer section */}
     </div>
   </div>
 );
--- Junkyard/src/components/Welcome.jsx
+++ Junkyard/src/components/Welcome.jsx
@@ -1,101 +1,32 @@
-import React, { useState } from "react";
-import { HiMenuAlt4 } from "react-icons/hi";
-import { AiOutlineClose } from "react-icons/ai";
-import { Link } from "react-router-dom";
-import walletConnectModule from "@web3-onboard/walletconnect";
-import injectedModule from "@web3-onboard/injected-wallets";
-import Onboard from "@web3-onboard/core";
-import logo from "../assets/logo.png";
+import React from "react";
+import logo1 from "../assets/banner.png";
 
-const walletConnect = walletConnectModule();
-const injected = injectedModule();
-
-const modules = [walletConnect, injected];
-
-const RPC_URL = "https://rpc.ankr.com/scroll_sepolia_testnet";
-
-const onboard = Onboard({
-  wallets: modules,
-  chains: [
-    {
-      id: "0x8274f",
-      token: "ETH",
-      namespace: "evm",
-      label: "Scroll Testnet",
-      rpcUrl: RPC_URL,
-    },
-  ],
-  appMetadata: {
-    name: "RECYCLE",
-    icon: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
-    description: "Recycle waste and save our environment",
-    recommendedInjectedWallets: [
-      { name: "Coinbase", url: "https://wallet.coinbase.com/" },
-      { name: "MetaMask", url: "https://metamask.io" },
-    ],
-  },
-});
-
-const NavBarItem = ({ title, classprops }) => (
-  <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
-);
-
-const Navbar = () => {
-  const [toggleMenu, setToggleMenu] = React.useState(false);
-  const [account, setAccount] = useState();
-
-  const connectWallet = async () => {
-    try {
-      const wallets = await onboard.connectWallet();
-      const { accounts } = wallets[0];
-      setAccount(accounts[0].address);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
+const Welcome = () => {
   return (
-    <nav className="w-full flex md:justify-center justify-between items-center p-4 navbar-bg">
-      <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <img src={logo} alt="logo" className="sm:w-10 lg:w-24 cursor-pointer" />
-      </div>
-      <ul className="text-white lg:text-3xl md:flex hidden items-center flex-initial">
-        <Link to="/" className="navbar-item">Home</Link>
-        <Link to="/create" className="navbar-item">Create-waste</Link>
-        <Link to="/explore" className="navbar-item">Marketplace</Link>
-        <div className="flex-grow"></div>
-        <button
-          type="button"
-          onClick={connectWallet}
-          className="flex flex-row justify-center items-center button-primary p-3 rounded-full cursor-pointer"
-        >
-          <p className="text-2xl font-semibold py-2 px-6">
-            {account ? `Connected: ${account.slice(0, 6)}...${account.slice(-4)}` : "Connect Wallet"}
+    <div className="flex w-full mf:flex-row flex-col justify-center items-center bg-gradient-to-r from-violet-700 to-pink-500 p-10">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 px-4">
+        <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
+          <h1 className="text-5xl sm:text-7xl text-white py-1 font-semibold">
+            GreenLink<br />
+          </h1>
+          <br />
+          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-4xl">
+            Waste Recycling & Sustainability <br /> Help our Environment <br />{" "}
+            and get rewarded
           </p>
-        </button>
-      </ul>
-      <div className="flex relative">
-        {!toggleMenu && (
-          <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
-        )}
-        {toggleMenu && (
-          <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
-        )}
-        {toggleMenu && (
-          <ul
-            className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-            flex flex-col justify-start items-end rounded-md navbar-bg text-white animate-slide-in"
-          >
-            <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
-            {["Home", "Create Waste", "Explore", "About"].map(
-              (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
-            )}
-          </ul>
-        )}
+          <br />
+          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-2xl">
+            Connect your wallet, submit details of your <br /> environmental
+            waste, recyclers go to the <br /> marketplace and recycle waste
+            close to their location...
+          </p>
+        </div>
       </div>
-    </nav>
+      <div className="sm:flex-[0.9] lg:flex-[0.9] flex-initial justify-left items-center">
+        <img src={logo1} alt="welcome" className="w-100 cursor-pointer" />
+      </div>
+    </div>
   );
 };
 
-export default Navbar;
+export default Welcome;
