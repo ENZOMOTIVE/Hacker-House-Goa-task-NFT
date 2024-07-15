5dc29d9f758385235f1f10aaba797fbd0e81f956
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-07-15 08:52:00 +0530

    contract addd

--- Junkyard/config.js
+++ Junkyard/config.js
@@ -1,4 +1,4 @@
 
 //export const wastemarketplaceAddress = "0x5A8c2d7BDcC65a751C1037f961fe6A41e6055120";
-export const wastemarketplaceAddress = ""; //CORE BLOCKCHAIN DEPLOY
+export const wastemarketplaceAddress = "0x0bdfac5569574038cf9c6e9f828aecc4b4dc1d82"; //CORE BLOCKCHAIN DEPLOY
 export const arbwastemarketplaceAddress = "0x5A8c2d7BDcC65a751C1037f961fe6A41e6055120";
